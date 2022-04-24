dc451e52185d50a285c8945a269f91c2ceeac0da
puddingii <gun4930@naver.com>
2022-04-24 22:39:00 +0900

    chore(eslint): eslint, prettier 적용

--- Frontend/src/components/Footer/Footer.js
+++ Frontend/src/components/Footer/Footer.js
@@ -15,47 +15,47 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
 
 class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer px-0 px-lg-3">
-        <Container fluid>
-          <nav>
-            <ul className="footer-menu">
-              <li>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Company
-                </a>
-              </li>
-              <li>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Portfolio
-                </a>
-              </li>
-              <li>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Blog
-                </a>
-              </li>
-            </ul>
-            <p className="copyright text-center">
-              © {new Date().getFullYear()}{" "}
-              <a href="http://www.creative-tim.com">Creative Tim</a>, made with
-              love for a better web
-            </p>
-          </nav>
-        </Container>
-      </footer>
-    );
-  }
+	render() {
+		return (
+			<footer className="footer px-0 px-lg-3">
+				<Container fluid>
+					<nav>
+						<ul className="footer-menu">
+							<li>
+								<a href="#pablo" onClick={e => e.preventDefault()}>
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="#pablo" onClick={e => e.preventDefault()}>
+									Company
+								</a>
+							</li>
+							<li>
+								<a href="#pablo" onClick={e => e.preventDefault()}>
+									Portfolio
+								</a>
+							</li>
+							<li>
+								<a href="#pablo" onClick={e => e.preventDefault()}>
+									Blog
+								</a>
+							</li>
+						</ul>
+						<p className="copyright text-center">
+							© {new Date().getFullYear()}{' '}
+							<a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a
+							better web
+						</p>
+					</nav>
+				</Container>
+			</footer>
+		)
+	}
 }
 
-export default Footer;
+export default Footer
