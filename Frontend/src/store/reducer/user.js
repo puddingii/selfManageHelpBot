5d5297b00091c50b4991ca67b5622c6f8f6adffa
puddingii <gun4930@naver.com>
2022-04-29 18:13:14 +0900

    chore(reducer): 기본 세팅 및 createAsyncThunk 예제코드 작성

--- Frontend/src/store/reducer/user.js
+++ Frontend/src/store/reducer/user.js
@@ -1,16 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+// https://api.bithumb.com/public/ticker/ALL
+
+/**
+ * 비동기 작업 예제코드
+ */
+export const fetchUserById = createAsyncThunk(
+	'user/fetchByIdStatus',
+	async (userId, thunkAPI) => {
+		const response = await axios('https://api.bithumb.com/public/ticker/ALL')
+		return response.data
+	},
+)
 
 export const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    value: 0,
-  },
-  reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-  },
-});
+	name: 'user',
+	initialState: {
+		value: 0,
+		temp: {},
+	},
+	reducers: {
+		increment: state => {
+			state.value += 1
+		},
+	},
+	extraReducers: builder => {
+		builder.addCase(fetchUserById.fulfilled, (state, action) => {
+			state.temp = action.payload
+		})
+	},
+})
 
-export const { increment } = userSlice.actions;
-export default userSlice.reducer;
+export const { increment } = userSlice.actions
+export default userSlice.reducer
