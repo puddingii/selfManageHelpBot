863023c1f60fbafd07c474b327227b2f556dac31
puddingii <gun4930@naver.com>
2022-04-30 17:35:59 +0900

    chore(createAsyncThunk): createSlice 옵션에 주는 방식 결정 및 테스트 완료

--- Frontend/src/store/reducer/user.js
+++ Frontend/src/store/reducer/user.js
@@ -50,24 +50,6 @@ export const userSlice = createSlice({
 				console.log('DefaultCase')
 			})
 	},
-	// extraReducers: {
-
-	// 	/** 데이터를 가져오기 전 */
-	// 	[fetchUserById.pending]: (state, action) => {
-	// 		console.log('Data pending')
-	// 		state.temp = action.payload
-	// 	},
-	// 	/** Data를 성공적으로 가져왔을 경우 */
-	// 	[fetchUserById.fulfilled]: (state, action) => {
-	// 		console.log('Data fulfilled')
-	// 		state.temp = action.payload
-	// 	},
-	// 	/** 데이터를 가져오는데 실패했을 경우 */
-	// 	[fetchUserById.rejected]: (state, action) => {
-	// 		console.log('Data rejected')
-	// 		state.temp = action.payload
-	// 	},
-	// },
 })
 
 export const { increment } = userSlice.actions
